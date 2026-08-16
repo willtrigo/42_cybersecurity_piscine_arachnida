7f5e6a3f825eca208085cbffbd5768e118873224
willtrigo <willtrigo@gmail.com>
2026-08-16 14:55:47 -0300

    Feat: implement parse that handle with -r -l -p  Changes to be committed: 	modified:   Dockerfile 	modified:   Makefile 	modified:   cmd/spider/main.go 	new file:   internal/config/config.go

--- ex01/cmd/spider/main.go
+++ ex01/cmd/spider/main.go
@@ -1,9 +1,36 @@
+// ************************************************************************** //
+//                                                                            //
+//                                                        :::      ::::::::   //
+//   main.go                                            :+:      :+:    :+:   //
+//                                                    +:+ +:+         +:+     //
+//   By: dande-je <dande-je@student.42sp.org.br>    +#+  +:+       +#+        //
+//                                                +#+#+#+#+#+   +#+           //
+//   Created: 2026/08/14 19:55:24 by dande-je          #+#    #+#             //
+//   Updated: 2026/08/16 10:52:25 by dande-je         ###   ########.fr       //
+//                                                                            //
+// ************************************************************************** //
+
 package main
 
 import (
 	"fmt"
+	"os"
+
+	"github.com/willtrigo/42_cybersecurity_piscine_arachnida/ex01/internal/config"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	if err := run(os.Args[0], os.Args[1:]); err != nil {
+		fmt.Fprintf(os.Stderr, "spider: %v\n", err)
+		os.Exit(1)
+	}
+}
+
+func run(progName string, args []string) error {
+	_, err := config.Parse(progName, args)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
---
+++ ex01/internal/config/config.go
@@ -0,0 +1,92 @@
+// ************************************************************************** //
+//                                                                            //
+//                                                        :::      ::::::::   //
+//   config.go                                          :+:      :+:    :+:   //
+//                                                    +:+ +:+         +:+     //
+//   By: dande-je <dande-je@student.42sp.org.br>    +#+  +:+       +#+        //
+//                                                +#+#+#+#+#+   +#+           //
+//   Created: 2026/08/16 10:15:06 by dande-je          #+#    #+#             //
+//   Updated: 2026/08/16 14:43:22 by dande-je         ###   ########.fr       //
+//                                                                            //
+// ************************************************************************** //
+
+// Package config parses and validates command-line arguments for the
+// spider crawler.
+package config
+
+import (
+	"errors"
+	"flag"
+	"fmt"
+)
+
+const (
+	DefaultOutputPath = "./data/"
+	DefaultMaxDepth   = 5
+)
+
+var (
+	// ErrMissingURL is returned when no URL argument is provided.
+	ErrMissingURL            = errors.New("config: missing required URL argument")
+	// ErrInvalidMaxDepth is returned when -l is not a positive integer.
+	ErrInvalidMaxDepth       = errors.New("config: -l must be a positive integer")
+	// ErrMaxDepthWithoutRecur is returned when -l is set without -r.
+	ErrMaxDepthWithourRecur = errors.New("config: -l requires -r to be set")
+)
+
+// Config holds the parsed and validated command-line configuration.
+type Config struct {
+	URL        string
+	OutputPath string
+	MaxDepth   int
+	Recursive  bool
+}
+
+// Parse parses args into a Config, returning an error if the URL is
+// missing or the flag combination is invalid.
+func Parse(progName string, args []string) (*Config, error) {
+	fs := flag.NewFlagSet(progName, flag.ContinueOnError)
+	fs.Usage = func() {
+		_, _ = fmt.Fprintf(fs.Output(), "Usage: %s [-r] [-l N] [-p PATH] URL\n\n", progName)
+		fs.PrintDefaults()
+	}
+
+	recursive := fs.Bool("r", false, "recursively download images found by following links")
+	maxDepth := fs.Int("l", DefaultMaxDepth, "maximum recursion depth(only used with -r)")
+	outputPath := fs.String("p", DefaultOutputPath, "directory downloaded images are saved to")
+
+	if err := fs.Parse(args); err != nil {
+		return nil, err
+	}
+
+	if fs.NArg() != 1 {
+		fs.Usage()
+		return nil, ErrMissingURL
+	}
+
+	var maxDepthSet bool
+	fs.Visit(func(f *flag.Flag) {
+		if f.Name == "l" {
+			maxDepthSet = true
+		}
+	})
+
+	if maxDepthSet && !*recursive {
+		return nil, ErrMaxDepthWithourRecur
+	}
+
+	if *recursive && *maxDepth <= 0 {
+		return nil, fmt.Errorf("%w: got %d", ErrInvalidMaxDepth, *maxDepth)
+	}
+
+	cfg := &Config{
+		URL:        fs.Arg(0),
+		Recursive:  *recursive,
+		OutputPath: *outputPath,
+	}
+	if cfg.Recursive {
+		cfg.MaxDepth = *maxDepth
+	}
+
+	return cfg, nil
+}
